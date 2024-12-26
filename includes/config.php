6633c06fa889f892a54178fdb74bb4139ad49eee
S-Hanaie <saami.h1988@gmail.com>
2024-12-26 20:54:04 +0330

    Update config.php
    test

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,13 @@
 <?php 
+// گرفتن متغیرهای محیطی از Railway
+$server = getenv('MYSQLHOST'); // Automatically set by Railway
+$user = getenv('MYSQLUSER'); // Automatically set by Railway
+$pass = getenv('MYSQL_ROOT_PASSWORD'); // Automatically set by Railway
+$dbname = getenv('MYSQLDATABASE'); // Automatically set by Railway
 
-$server = getenv('MYSQLHOST'); 
-$user = getenv('MYSQLUSER'); 
-$pass = getenv('MYSQL_ROOT_PASSWORD'); 
-$dbname = getenv('MYSQLDATABASE'); 
-
-
+// Establish database connection.
 try {
-    
+    // استفاده از متغیرهای محیطی در رشته اتصال
     $dbh = new PDO("mysql:host=$server;dbname=$dbname", $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'"));
 } catch (PDOException $e) {
     exit("Error: " . $e->getMessage());
